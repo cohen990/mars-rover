e7be6a4010e599d33b04bbe90a276a4fc6cf8d5b
Samantha Cohen <samanthacohen294@gmail.com>
2025-12-06 17:19:24 +0000

    I think this was 374 -> 376

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
+const DX = [0, 1, 0, -1];
+const DY = [1, 0, -1, 0];
+
 export function commandRover(instructions: string): string {
   const lines = instructions.split("\n");
   const results = [];
   
   for (let i = 1; i < lines.length; i += 2) {
     const parts = lines[i].split(" ");
-    let posX = +parts[0];
-    let posY = +parts[1];
-    let dir = parts[2].charCodeAt(0) === 78 ? 0 : parts[2].charCodeAt(0) === 69 ? 1 : parts[2].charCodeAt(0) === 83 ? 2 : 3;
+    let x = +parts[0], y = +parts[1], d = 'NESW'.indexOf(parts[2]);
     
     for (const command of lines[i + 1]) {
       if (command === 'M') {
-        posX += dir & 1 ? (dir & 2 ? -1 : 1) : 0;
-        posY += dir & 1 ? 0 : (dir & 2 ? -1 : 1);
+        x += DX[d];
+        y += DY[d];
       } else {
-        dir = (dir + (command === 'L' ? 3 : 1)) & 3;
+        d = (d + (command === 'L' ? 3 : 1)) & 3;
       }
     }
     
-    results.push(`${posX} ${posY} ${'NESW'[dir]}`);
+    results.push(x + " " + y + " " + 'NESW'[d]);
   }
   
   return results.join("\n");
