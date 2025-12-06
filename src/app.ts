6ecf6b030a8cadf80128e85d97945621fe5eaa48
Samantha Cohen <samanthacohen294@gmail.com>
2025-12-06 17:08:02 +0000

    414 -> 374!

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,23 @@
-const DIRECTIONS = ['N', 'E', 'S', 'W'];
-const MOVES = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+const DIRECTIONS = 'NESW';
+const DELTA_X = [0, 1, 0, -1];
+const DELTA_Y = [1, 0, -1, 0];
 
 export function commandRover(instructions: string): string {
   const lines = instructions.split("\n");
   const results = [];
   
   for (let i = 1; i < lines.length; i += 2) {
-    const [x, y, heading] = lines[i].split(" ");
-    let [posX, posY, dirIndex] = [+x, +y, DIRECTIONS.indexOf(heading)];
+    const parts = lines[i].split(" ");
+    let posX = +parts[0];
+    let posY = +parts[1];
+    let dirIndex = DIRECTIONS.indexOf(parts[2]);
     
     for (const command of lines[i + 1]) {
       if (command === 'M') {
-        posX += MOVES[dirIndex][0];
-        posY += MOVES[dirIndex][1];
+        posX += DELTA_X[dirIndex];
+        posY += DELTA_Y[dirIndex];
       } else {
-        dirIndex = (dirIndex + (command === 'L' ? 3 : 1)) % 4;
+        dirIndex = (dirIndex + (command === 'L' ? 3 : 1)) & 3;
       }
     }
     
