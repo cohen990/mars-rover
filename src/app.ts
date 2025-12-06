78f12ca1e2baac9b7ac674444e01095aa7f6b29a
Samantha Cohen <samanthacohen294@gmail.com>
2025-12-06 17:37:25 +0000

    revert to optimal

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,27 @@
+const DIRECTIONS = 'NESW';
+const DELTA_X = [0, 1, 0, -1];
+const DELTA_Y = [1, 0, -1, 0];
+
 export function commandRover(instructions: string): string {
   const lines = instructions.split("\n");
   const results = [];
   
   for (let i = 1; i < lines.length; i += 2) {
     const parts = lines[i].split(" ");
-    let x = +parts[0], y = +parts[1], d = 'NESW'.indexOf(parts[2]);
+    let posX = +parts[0];
+    let posY = +parts[1];
+    let dirIndex = DIRECTIONS.indexOf(parts[2]);
     
-    const commands = lines[i + 1];
-    for (let j = 0; j < commands.length; j++) {
-      const cmd = commands[j];
-      if (cmd === 'M') {
-        if (d & 1) x += d === 1 ? 1 : -1;
-        else y += d === 0 ? 1 : -1;
+    for (const command of lines[i + 1]) {
+      if (command === 'M') {
+        posX += DELTA_X[dirIndex];
+        posY += DELTA_Y[dirIndex];
       } else {
-        d = cmd === 'L' ? (d + 3) & 3 : (d + 1) & 3;
+        dirIndex = (dirIndex + (command === 'L' ? 3 : 1)) & 3;
       }
     }
     
-    results.push(x + " " + y + " " + 'NESW'[d]);
+    results.push(`${posX} ${posY} ${DIRECTIONS[dirIndex]}`);
   }
   
   return results.join("\n");
